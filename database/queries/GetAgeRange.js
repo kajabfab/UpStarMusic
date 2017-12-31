ba95edc2cd8ff2293941071215af88fe2a482b16
kajabfab <vishal.bfab@gmail.com>
2017-12-31 16:24:17 +0530

    EditArtist GetAgeRange GetYearsActiveRange SearchArtists

--- database/queries/GetAgeRange.js
+++ database/queries/GetAgeRange.js
@@ -2,8 +2,26 @@ const Artist = require('../models/artist');
 
 /**
  * Finds the lowest and highest age of artists in the Artist collection
- * @return {promise} A promise that resolves with an object
+ * @return {Promise<[any , any]>} A promise that resolves with an object
  * containing the min and max ages, like { min: 16, max: 45 }.
  */
 module.exports = () => {
+    const minQuery = Artist
+        .find({})
+        .sort({age: 1})
+        .limit(1)
+        .then(artists => artists[0].age);
+
+    const maxQuery = Artist
+        .find({})
+        .sort({age: -1})
+        .limit(1)
+        .then(artists => artists[0].age);
+
+    return Promise.all([
+        minQuery,
+        maxQuery
+    ]).then((ages) => {
+        return {min: ages[0], max: ages[1]};
+    });
 };
--- database/queries/SearchArtists.js
+++ database/queries/SearchArtists.js
@@ -4,9 +4,44 @@ const Artist = require('../models/artist');
  * Searches through the Artist collection
  * @param {object} criteria An object with a name, age, and yearsActive
  * @param {string} sortProperty The property to sort the results by
- * @param {integer} offset How many records to skip in the result set
- * @param {integer} limit How many records to return in the result set
- * @return {promise} A promise that resolves with the artists, count, offset, and limit
+ * @param {number} offset How many records to skip in the result set
+ * @param {number} limit How many records to return in the result set
+ * @return {Promise<[any , any]>} A promise that resolves with the artists, count, offset, and limit
  */
 module.exports = (criteria, sortProperty, offset = 0, limit = 20) => {
+
+    let searchObject = {};
+    searchObject.name = {$regex: criteria.name};
+    if (criteria.age !== undefined) {
+        searchObject.age = {
+            $gte: criteria.age.min,
+            $lte: criteria.age.max
+        };
+    }
+    if (criteria.yearsActive !== undefined) {
+        searchObject.yearsActive = {
+            $gte: criteria.yearsActive.min,
+            $lte: criteria.yearsActive.max
+        };
+    }
+
+
+    const query = Artist
+        .find(searchObject)
+        .sort({[sortProperty]: 1})
+        .skip(offset)
+        .limit(limit);
+
+    return Promise.all([
+            query,
+            Artist.count()
+        ])
+        .then((result) => {
+            return {
+                all: result[0],
+                count: result[1],
+                offset,
+                limit
+            };
+        });
 };
